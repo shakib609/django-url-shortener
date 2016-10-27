fd6b81a8452a835b7014e57c0045a1c938fdd76e
Shakib Hossain <shakib609@gmail.com>
2016-10-27 17:35:35 +0600

    Add form_handler for creating shortened urls

--- apps/shortener/urls.py
+++ apps/shortener/urls.py
@@ -3,8 +3,21 @@ from . import views
 
 app_name = 'shortener'
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'^(?P<short_url>[\w_\-\d]+)$',
+    url(
+        r'^$',
+        views.homepage,
+        name='homepage'
+    ),
+
+    url(
+        r'^shorten$',
+        views.form_handler,
+        name='form_handler'
+    ),
+
+    url(
+        r'^(?P<short_url>[\w_\-\d]+)$',
         views.short_url_redir,
-        name='short_url_redir'),
+        name='short_url_redir'
+    ),
 ]
--- apps/shortener/views.py
+++ apps/shortener/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.views.decorators.http import require_POST
+
 
 from .forms import ShortURLForm
 from .models import ShortURL
@@ -9,6 +11,24 @@ from .models import ShortURL
 def homepage(request):
     form = ShortURLForm()
     return render(request, 'shortener/homepage.html', {'form': form})
+
+
+@require_POST
+def form_handler(request):
+    form = ShortURLForm(request.POST)
+    if form.is_valid():
+        url = form.cleaned_data['url']
+        short_url = form.cleaned_data['short_url']
+        if short_url:
+            urls = ShortURL.objects.filter(short_url=short_url)
+            if len(urls) > 0:
+                messages.error(
+                    request,
+                    'The short_url exists! Try something else.')
+                return redirect(reverse('shortener:homepage'))
+        s_url = ShortURL.objects.create(url=url, short_url=short_url)
+        messages.success(request, 'URL Shortened successfully!')
+        return redirect(reverse('shortener:homepage'))
 
 
 def short_url_redir(request, short_url):
