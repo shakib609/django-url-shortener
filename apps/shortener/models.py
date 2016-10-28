1868a2b5da0d6b6707311b27793644b215c9a273
Shakib Hossain <shakib609@gmail.com>
2016-10-28 12:39:47 +0600

    Add db_index to ShortURL.short_url field

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ShortURL(models.Model):
     url = models.URLField(default=None)
-    short_url = models.CharField(max_length=128)
+    short_url = models.CharField(max_length=128, db_index=True)
 
     def __str__(self):
         return self.url
